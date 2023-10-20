541e2baa10c8f65eb033fe8398475f1d571bc3a0
Arthur Béquié <arthurbequie@protonmail.com>
2023-10-20 17:18:03 +0200

    Adding example of working one news dateime attribut retrieve

--- v3/scraper/main.py
+++ v3/scraper/main.py
@@ -25,9 +25,36 @@ try:
         EC.presence_of_element_located((By.CLASS_NAME, "news"))
     )
     # Perform any interactions
-    print(driver.title)
-    news = driver.find_elements(By.CLASS_NAME, "news-row")
-    print(news)
+    # print(driver.title)
+    news_rows = driver.find_elements(By.CLASS_NAME, "news-row")
+
+    for news_row in news_rows:
+        # Extract required information
+        time_element = news_row.find_element(By.XPATH, "//div[@class='news-cells']")
+        time_url = time_element.find_element(By.CLASS_NAME, "nc-date")
+        time_container = time_url.find_element(By.TAG_NAME, "span")
+        time_tag_element = time_container.find_element(By.XPATH, "//time")
+        time = time_tag_element.get_attribute('datetime')
+
+        # title_element = news_row.find_element(By.CSS_SELECTOR, '.news-cell.nc-title span.title-text span')
+        # title = title_element.text
+
+        # source_element = news_row.find_element(By.CSS_SELECTOR, '.si-source-domain')
+        # source = source_element.text
+
+        # currency_elements = news_row.find_elements(By.CSS_SELECTOR, '.news-cell.nc-currency .colored-link')
+        # currencies = [el.text for el in currency_elements]
+
+        #Create the news object (dictionary)
+        news_object = {
+            'Time': time,
+            # 'Title': title,
+            # 'Source': source,
+            # 'Currencies': currencies
+        }
+
+        # Output the news object
+        print(news_object)
 finally:
     # else quit
     driver.quit()
