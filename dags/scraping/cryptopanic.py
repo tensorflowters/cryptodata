7b6d65427a5b8df78950ed0931457bf9bf645c9e
Arthur Béquié <arthurbequie@protonmail.com>
2024-01-19 16:26:05 +0100

    cleaning repo and bug

--- dags/scraping/cryptopanic.py
+++ dags/scraping/cryptopanic.py
@@ -1,32 +1,32 @@
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
-with DAG('scrap_cryptopanic',
-         default_args=default_args,
-         schedule_interval=timedelta(minutes=5),
-         start_date=datetime(2023, 10, 20),
-         catchup=False) as dag:
-
+with DAG(
+    "scrap_cryptopanic",
+    default_args=default_args,
+    schedule_interval=timedelta(minutes=5),
+    start_date=datetime(2023, 10, 20),
+    catchup=False,
+) as dag:
     run_scraper_task = DockerOperator(
-        task_id='run_scraper_task',
-        image='v3-scraper:latest',
-        api_version='auto',
+        task_id="run_scraper_task",
+        image="cryptodata-scraper:latest",
+        api_version="auto",
         command="python main.py",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="v3_default",
+        docker_url="tcp://docker-proxy:2375",
+        network_mode="cryptodata_default",
         auto_remove="force",
-        environment={
-            'KAFKA_BROKER': 'kafka:9092'
-        }
+        environment={"KAFKA_BROKER": "kafka:9092"},
     )
--- scrapers/example/EXAMPLE.md
+++
@@ -1,1 +0,0 @@
-# Define other website scraping python project in this directory
