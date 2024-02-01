ce316a37e6b204f357f38c583f2af5463c74e359
Hugo Mortreux <70602545+hxjo@users.noreply.github.com>
2024-02-01 16:58:12 +0100

    init poetry

--- db/initdb.py
+++ db/initdb.py
@@ -1,27 +1,39 @@
 import os
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ARRAY, inspect
+
+from sqlalchemy import (
+    ARRAY,
+    Column,
+    Integer,
+    MetaData,
+    String,
+    Table,
+    create_engine,
+    inspect,
+)
 
 # Connect to the database
-db_name = os.getenv('POSTGRES_DB')
-db_pwd = os.getenv('POSTGRES_PASSWORD')
-db_u = os.getenv('POSTGRES_USER')
+db_name = os.getenv("POSTGRES_DB")
+db_pwd = os.getenv("POSTGRES_PASSWORD")
+db_u = os.getenv("POSTGRES_USER")
 
-engine = create_engine(f'postgresql://{db_u}:{db_pwd}@client_db/{db_name}')
+engine = create_engine(f"postgresql://{db_u}:{db_pwd}@client_db/{db_name}")
 metadata = MetaData()
 
 
 if not inspect(engine).has_table("scraped_websites"):
     print("Table does not exist, creating it...")
-    table = Table('scraped_websites', metadata,
-                  Column('id', Integer, primary_key=True),
-                  Column('currencies', ARRAY(String)),
-                  Column('hashed_url', String),
-                  Column('link_page', String),
-                  Column('published_at', String),
-                  Column('publish_from_when_scraped', String),
-                  Column('source_domain', String),
-                  Column('title', String),
-                  )
+    table = Table(
+        "scraped_websites",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("currencies", ARRAY(String)),
+        Column("hashed_url", String, unique=True),
+        Column("link_page", String),
+        Column("published_at", String),
+        Column("publish_from_when_scraped", String),
+        Column("source_domain", String),
+        Column("title", String),
+    )
     table.create(engine)
 else:
     print("Table already exists")
