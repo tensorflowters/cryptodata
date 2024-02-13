53c422a2615c0e08f49c251f38fe9349cacd9a1e
Arthur Béquié <arthurbequie@protonmail.com>
2024-02-13 06:46:49 +0100

    [WIP] Writing kubernetes manifest for airflow services and db.

--- commands/kube.mk
+++ commands/kube.mk
@@ -7,13 +7,24 @@ k8s_services:
 k8s_help:
 	@microk8s kubectl --help
 
-k8s_apply_airflow_webserver:
+k8s_apply_airflow:
 	@kubectl apply -f airflow-config-pvc.yml
+	@kubectl apply -f airflow-init-script-configmap.yml
 	@kubectl apply -f airflow-configmap.yml
 	@kubectl apply -f airflow-dags-pvc.yml
 	@kubectl apply -f airflow-logs-pvc.yml
 	@kubectl apply -f airflow-plugins-pvc.yml
 	@kubectl apply -f airflow-secrets.yml
+	@kubectl apply -f airflow-worker-deployment.yml
+	@kubectl apply -f airflow-scheduler-deployment.yml
+	@kubectl apply -f airflow-triggerer-deployment.yml
 	@kubectl apply -f airflow-webserver-deployment.yml
 	@kubectl apply -f airflow-webserver-service.yml
 	@kubectl apply -f airflow-webserver-ingress.yml
+	@kubectl apply -f airflow-cli-job.yml
+
+k8s_apply_airflow_db:
+	@kubectl apply -f airflow-db-secrets.yml
+	@kubectl apply -f airflow-db-pvc.yml
+	@kubectl apply -f airflow-db-deployment.yml
+	@kubectl apply -f airflow-db-service.yml
