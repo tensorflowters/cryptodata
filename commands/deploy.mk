56c8c9545bb8d258201d40dd7a0bfff12fd98a0e
Arthur Béquié <arthurbequie@protonmail.com>
2024-02-13 03:04:05 +0100

    [WIP] I'm pretty sure that you have better to do than look at this commit

--- commands/deploy.mk
+++ commands/deploy.mk
@@ -1,8 +1,14 @@
-include env-vars/.env.prod
-export $(shell sed 's/=.*//' env-vars/.env.prod)
+ENV?=env-vars/.env.ansible.prod
 
-print_ansible_home:
-	@echo ${ANSIBLE_HOME}
+include $(ENV)
+export $(shell sed 's/=.*//' $(ENV))
 
-check_host_python:
-	@cd ./ansible && poetry run ansible -m command -a "python -V" k8s_cluster_01
+DOCKER_CMD=docker compose -p cryptodata_deployment -f docker/docker-compose-ansible.prod.yml --env-file $(ENV)
+
+gen_ssh_key:
+	@/bin/bash -c "ssh-keygen -q -t ed25519 -N '${SERVER_03100_USER_SSH_PASS}' -f deployment/ansible/.ssh_config/${SERVER_03100_SSH_KEY_NAME} <<<y >/dev/null 2>&1"
+	@chown -R 1000:1000 deployment/ansible/.ssh_config
+	@chmod 600 deployment/ansible/.ssh_config/${SERVER_03100_SSH_KEY_NAME}
+
+deploy:
+	@${DOCKER_CMD} up -d --build
